6f563949f9fdfd00ea0412a79dae2d775f52d35c
sulei <1134283134@qq.com>
2016-03-04 21:31:49 +0800

    * 将全部房源名称改为：_base_house_all + 加入我的房源模块

---
+++ js/base_house_my.js
@@ -0,0 +1,49 @@
+$.init();
+
+// 添加'refresh'监听器
+$(document).on('refresh', '.pull-to-refresh-content', function(e) {
+
+	refreshData();
+});
+
+
+function refreshData() {
+
+
+	var data = getSignData();
+	data.m = getMoblieNo();
+	data.u = getAuth();
+
+	console.log(data);
+
+	$.ajax({
+		type: 'GET',
+		url: getRemoteSite() + '/api/get_house_all',
+		data: data,
+		contentType: "application/json",
+		dataType: 'jsonp',
+		timeout: 10000,
+		success: function(data, status, xhr) {
+			if (data.HasError) {
+				$.alert(data.Errors.join(","));
+			} else {
+				var html = template('houses', data);
+				document.getElementById("house-list").innerHTML = html;
+			}
+		},
+		error: function(xhr, type, error) {
+			if ("timeout" === type) {
+				$.alert("网络请求超时，请检查设备信号后重试。");
+			} else {
+				$.alert('网络请求错误，' + type + "," + error);
+			}
+		},
+		complete: function(xhr, status) {
+			//alert("complete");
+		}
+	}).send();
+}
+
+$(function() {
+	refreshData();
+});
