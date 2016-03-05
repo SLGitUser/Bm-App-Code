db7c75d463a08c5aa811f2aba8ee89e87633b5a0
sulei <1134283134@qq.com>
2016-03-05 15:48:14 +0800

    * 楼盘明细模块完善

---
+++ js_br/base_house_detail.js
@@ -0,0 +1,50 @@
+$.init();
+
+// 添加'refresh'监听器
+$(document).on('refresh', '.pull-to-refresh-content', function(e) {
+
+	refreshData();
+});
+
+
+function refreshData() {
+
+
+	var data = getSignData();
+	data.houseId = getlocal("houseId");
+	data.u = getAuth();
+
+console.info("显示"+data.houseId);
+
+	$.ajax({
+		type: 'GET',
+		url: getRemoteSite() + '/api/get_house_detail',
+		data: data,
+		contentType: "application/json",
+		dataType: 'jsonp',
+		timeout: 10000,
+		success: function(data, status, xhr) {
+			if (data.HasError) {
+				$.alert(data.Errors.join(","));
+			} else {
+				console.info(data);
+				var html = template('is_message', data);
+				document.getElementById("message").innerHTML = html;
+			}
+		},
+		error: function(xhr, type, error) {
+			if ("timeout" === type) {
+				$.alert("网络请求超时，请检查设备信号后重试。");
+			} else {
+				$.alert('网络请求错误，' + type + "," + error);
+			}
+		},
+		complete: function(xhr, status) {
+			//alert("complete");
+		}
+	});
+}
+
+$(function() {
+	refreshData();
+});
