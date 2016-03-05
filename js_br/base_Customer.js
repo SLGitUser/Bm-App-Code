772ef7777ded3f91a05bdac183dab15dd0dda4f6
zzyao <zzy6606076@163.com>
2016-03-05 16:26:33 +0800

    *

--- js_br/base_Customer.js
+++ js_br/base_Customer.js
@@ -1,20 +1,19 @@
 $.init();
-
 // 添加'refresh'监听器
 $(document).on('refresh', '.pull-to-refresh-content', function(e) {
 
 	refreshData();
 });
-function Customer(){
+
+function refreshData(){
 	
 	var data = getSignData();
 	data.m = getMoblieNo();
-
 	console.log(data);
     $.ajax({
 		type: 'GET',
 		// http://123.56.185.114:8002/api/account?app=BEA&time=1456669504149&sign=c16f3826ee62405e4d24f19b8fa07911&m=18600000000&p=123456&_=1456669504153&callback=jsonp1
-		url: getRemoteSite() + 'api/base_Customer',
+		url: getRemoteSite() + 'api/base_customer_details',
 		// data to be added to query string:
 		data: data,
 		contentType: "application/json",
@@ -32,14 +31,15 @@ function Customer(){
 			if (data.HasError) {
 				$.alert(data.Errors.join(","));
 			}else {
+				alert("....");
 				document.getElementById("activity-tuike-count").innerText = data.Model.TuikeAmount;
 				document.getElementById("active-user-count").innerText = data.Model.ActiveCustomer;
 				document.getElementById("total-sales-amount").innerText = data.Model.TotalSales;
 				document.getElementById("user-task-count").innerText = data.Model.AlertCount;
 				$.pullToRefreshDone('.pull-to-refresh-content'); 
-//				var html = template('cpl', data.Model);
-//				document.getElementById('cpl-c').innerHTML = html;
-			}
+			    var html = template('cpl',Date.model);
+                document.getElementById('cpl-c').innerHTML = html;
+	 		}
 		},
 		error: function(xhr, type, error) {
 			if ("timeout" === type) {
@@ -51,12 +51,10 @@ function Customer(){
 		complete: function(xhr, status) {
 			//alert("complete");
 		}
-	}).send();
+	});
 }
 
 $(function() {
-	refreshData();
-
+refreshData();
 });
     
-}
