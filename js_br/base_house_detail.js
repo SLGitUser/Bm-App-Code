632ad5cc872db4418befc647dab4fa87ab5291d7
sulei <1134283134@qq.com>
2016-03-06 16:21:23 +0800

    * 楼房信息模块完善

--- js_br/base_house_detail.js
+++ js_br/base_house_detail.js
@@ -1,12 +1,50 @@
 $.init();
+//添加'refresh'监听器
+$(document).on('refresh', '.pull-to-refresh-content', function(e) {
 
-// 添加'refresh'监听器
-//$(document).on('refresh', '.pull-to-refresh-content', function(e) {
-//
-//	refreshData();
-//});
+	refreshData();
+});
 
+//切换收藏状态
+function changeCollect(){
+	var qs = getQueryStrings();
+	var data = getSignData();
+	data.u = getAccountNo();
+	data.h = qs["no"];
+	$.ajax({
+		type: 'GET',
+		url: getRemoteSite() + '/api/collect_house',
+		data: data,
+		contentType: "application/json",
+		dataType: 'jsonp',
+		timeout: 10000,
+		success: function(data, status, xhr) {
+			if (data.HasError) {
+				$.alert(data.Errors.join(","));
+			} else {
+				console.info(data+"     "+data.Result);
+				if(data.Result.toString()==="false"){
+					$("#star").removeClass("icon-star");
+					$("#star").addClass("icon-card");
+				}else{
+					$("#star").removeClass("icon-card");
+					$("#star").addClass("icon-star");
+				}
+			}
+		},
+		error: function(xhr, type, error) {
+			if ("timeout" === type) {
+				$.alert("网络请求超时，请检查设备信号后重试。");
+			} else {
+				$.alert('网络请求错误，' + type + "," + error);
+			}
+		},
+		complete: function(xhr, status) {
+		}
+	});
+}
 
+//刷新数据
 function refreshData() {
 
 	var qs = getQueryStrings();
@@ -28,10 +66,13 @@ function refreshData() {
 				$.alert(data.Errors.join(","));
 			} else {
 				data.Model.AddrPic = getPicUrl()+data.Model.AddrPic;
-				
+				data.Model.From = data.Model.From.toString();
+				data.Model.Clinch = data.Model.Clinch.toString();
+				data.Model.See = data.Model.See.toString();
+				data.Model.NeedSee = data.Model.NeedSee.toString();
+				data.Model.NeedSeeThirty = data.Model.NeedSeeThirty.toString();
 				console.info(data);
-				console.info(data.AddrPic);
-				var html = template('is_message', data);
+				var html = template('is_message', data.Model);
 				document.getElementById("message").innerHTML = html;
 				document.getElementById("abc").src = data.Model.AddrPic;
 			}
@@ -46,6 +87,9 @@ function refreshData() {
 		complete: function(xhr, status) {
 			//alert("complete");
 		}
+	});
+	$("#star").click(function(){
+		changeCollect();
 	});
 }
 
